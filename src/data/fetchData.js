87a71a29ebbe5dbc38b05ff2de64fa8912d1681f
Yosri Swidan <66914036+ImYosrii@users.noreply.github.com>
2022-08-19 18:53:53 +0300

    Update fetchData.js

--- src/data/fetchData.js
+++ src/data/fetchData.js
@@ -1,15 +1,14 @@
 const apiAuthorize = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'e1663eba67msh767268257c50a15p1e3c4ejsn7824421f964c',
+		'X-RapidAPI-Key': '9622861093msh398bf11fe4ca399p11812ejsnfff9a3a80a82',
 		'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
 	}
 }
-// more key 9622861093msh398bf11fe4ca399p11812ejsnfff9a3a80a82
 const apiAuthorizeYoutube = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'e1663eba67msh767268257c50a15p1e3c4ejsn7824421f964c',
+		'X-RapidAPI-Key': '9622861093msh398bf11fe4ca399p11812ejsnfff9a3a80a82',
 		'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
 	}
 };
